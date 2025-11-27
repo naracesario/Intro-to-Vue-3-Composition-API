6883a7a622343cf166df30e9d5674fcaec684a34
ncesario <nlmcesario@gmail.com>
2025-11-26 23:44:15 -0300

    Added url binding

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
+const myUrl = 'https://google.com'
 
 </script>
   
@@ -16,6 +17,7 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <a :href="myUrl">Click</a>
       </div>
     </div>
   </div>
